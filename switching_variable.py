5b2af34d4f8ed23d1af8258713bdf8c608d20c42
nooreen <nooreenamirjan@gmail.com>
2022-04-14 17:53:21 +0200

    switches values of two variables

---
+++ switching_variable.py
@@ -0,0 +1,9 @@
+var_1=input('Enter value for variable 1: ')
+var_2=input('Enter value for variable 2: ')
+
+temp=var_2
+var_2=var_1
+var_1=temp
+
+print ("variable 1:", var_1)
+print("variable 2:",var_2)
