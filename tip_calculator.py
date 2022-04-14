11bc6c96b3dabb9f88f345063176e029fd5cf71d
nooreen <nooreenamirjan@gmail.com>
2022-04-14 21:09:22 +0200

    Splits the amount of tip among people.

---
+++ tip_calculator.py
@@ -0,0 +1,9 @@
+total_bill=float(input("What is the total bill? $"))
+tip_percent=float(input("What percentage would you like to give tip? %"))
+total_people=float(input("How many people to split bill?"))
+
+tip_amount=((tip_percent)/100)*total_bill
+tip_per_person=tip_amount/total_people
+
+print( "Each person should pay",tip_per_person)
+
