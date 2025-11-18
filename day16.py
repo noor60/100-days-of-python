18330d06772c185fdedf7740706eece48efc6754
nooreen <nooreenamirjan@gmail.com>
2025-11-18 22:41:06 +0100

    oop

---
+++ day16.py
@@ -0,0 +1,14 @@
+import turtle
+# turtle graphics
+tur=turtle.Turtle()
+
+tur.shape('turtle')
+tur.color('coral')
+screen=turtle.Screen()
+tur.speed(1)
+
+tur.forward(250)
+tur.right(90)
+tur.forward(100)
+screen.exitonclick()
+screen.mainloop()
